7083e78cc932c585b812db1b53d301fd18d0dcf7
Kumar Kartikay <krkartikay@gmail.com>
2015-01-28 14:13:15 +0530

    Cleaned up code a little

--- client.py
+++ client.py
@@ -7,18 +7,16 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((server,PORT))
 
 def main():
-	while 1:
-		a = s.recv(2048)
-		print "Recieved task. Processing task."
-		print a
-		r = process_task(a)
-		print "Result is, ", r
-		s.send(str(r))
-		print "Asking for next task."
+    while 1:
+        a = s.recv(2048)
+        print "Recieved task. Processing task."
+        r = process_task(a)
+        print "Result is, ", r
+        s.send(str(r))
+        print "Asking for next task."
 
 def process_task(t):
-	print "Executing , \"%s\"."%t
-	exec t # Should define r
-	return r
+    exec t # Should define r
+    return r
 
 main()
--- server.py
+++ server.py
@@ -6,17 +6,17 @@ serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.bind(('', PORT))
 
 def main():
-	serversocket.listen(5)
-	(clientsocket, address) = serversocket.accept()
-	while 1:
-		provide_task(clientsocket)
-	clientsocket.close()
+    serversocket.listen(5)
+    (clientsocket, address) = serversocket.accept()
+    while 1:
+        provide_task(clientsocket)
+    clientsocket.close()
 
 def provide_task(s):
-	print "Recieved connection. Sending task."
-	task = "r=1+1\n" # Must define r and end with 1010011010
-	print s.send(task)
-	result = s.recv(2048)
-	print "Recieved result. Result is, " , result
+    print "Recieved connection. Sending task."
+    task = "r=1+1\n" # Must define r and end with 1010011010
+    print s.send(task)
+    result = s.recv(2048)
+    print "Recieved result. Result is, " , result
 
 main()
